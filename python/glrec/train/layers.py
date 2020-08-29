e1ca1c47a595802883b0769582ec52e538205cc4
hav4ik <hav4ik@users.noreply.github.com>
2020-08-29 06:58:01 -0700

    Added AdaCos and CosFace layers

--- python/glrec/train/layers.py
+++ python/glrec/train/layers.py
@@ -2,12 +2,14 @@ import math
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras.layers import Layer
+from tensorflow.keras.initializers import Constant
 from tensorflow.python.keras.utils import tf_utils
 
 
 class ArcMarginProduct(Layer):
     """
-    Implements large margin arc distance.
+    Implements large margin arc distance. This implementation was taken from
+    Kaggle (https://www.kaggle.com/akensert/glrec-resnet50-arcface-tf2-2).
 
     Reference:
         https://arxiv.org/pdf/1801.07698.pdf
@@ -22,32 +24,32 @@ class ArcMarginProduct(Layer):
                  ls_eps=0.0,
                  **kwargs):
 
-        super(ArcMarginProduct, self).__init__(**kwargs)
-        self.n_classes = num_classes
-        self.s = s
-        self.m = m
-        self.ls_eps = ls_eps
-        self.easy_margin = easy_margin
-        self.cos_m = tf.math.cos(m)
-        self.sin_m = tf.math.sin(m)
-        self.th = tf.math.cos(math.pi - m)
-        self.mm = tf.math.sin(math.pi - m) * m
+        super().__init__(**kwargs)
+        self._n_classes = num_classes
+        self._s = s
+        self._m = m
+        self._ls_eps = ls_eps
+        self._easy_margin = easy_margin
+        self._cos_m = tf.math.cos(m)
+        self._sin_m = tf.math.sin(m)
+        self._th = tf.math.cos(math.pi - m)
+        self._mm = tf.math.sin(math.pi - m) * m
 
     def build(self, input_shape):
         embedding_shape, label_shape = input_shape
-        self.W = self.add_weight(name='W',
-                                 shape=(embedding_shape[-1], self.n_classes),
-                                 initializer='glorot_uniform',
-                                 dtype='float32',
-                                 trainable=True,
-                                 regularizer=None)
+        self._W = self.add_weight(name='W',
+                                  shape=(embedding_shape[-1], self._n_classes),
+                                  initializer='glorot_uniform',
+                                  dtype='float32',
+                                  trainable=True,
+                                  regularizer=None)
 
     def call(self, inputs, training=None):
         X, y = inputs
         y = tf.cast(y, dtype=tf.int32)
         cosine = tf.matmul(
             tf.math.l2_normalize(X, axis=1),
-            tf.math.l2_normalize(self.W, axis=0)
+            tf.math.l2_normalize(self._W, axis=0)
         )
 
         training = _resolve_training(self, training)
@@ -56,21 +58,21 @@ class ArcMarginProduct(Layer):
             return cosine
         else:
             sine = tf.math.sqrt(1.0 - tf.math.pow(cosine, 2))
-            phi = cosine * self.cos_m - sine * self.sin_m
-            if self.easy_margin:
+            phi = cosine * self._cos_m - sine * self._sin_m
+            if self._easy_margin:
                 phi = tf.where(cosine > 0, phi, cosine)
             else:
-                phi = tf.where(cosine > self.th, phi, cosine - self.mm)
+                phi = tf.where(cosine > self._th, phi, cosine - self._mm)
             one_hot = tf.cast(
-                tf.one_hot(y, depth=self.n_classes),
+                tf.one_hot(y, depth=self._n_classes),
                 dtype=cosine.dtype
             )
-            if self.ls_eps > 0:
-                one_hot = (1 - self.ls_eps) * one_hot + \
-                        self.ls_eps / self.n_classes
+            if self._ls_eps > 0:
+                one_hot = (1 - self._ls_eps) * one_hot + \
+                        self._ls_eps / self._n_classes
 
             output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
-            output *= self.s
+            output *= self._s
             return output
 
 
@@ -83,7 +85,13 @@ class ArcFace(Layer):
       PyTorch implementation:
         https://github.com/4uiiurz1/pytorch-adacos/blob/master/metrics.py
     """
-    def __init__(self, num_classes, s=30.0, m=0.5, regulazer=None, **kwargs):
+    def __init__(self,
+                 num_classes,
+                 s=30.0,
+                 m=0.5,
+                 regulazer=None,
+                 **kwargs):
+
         super().__init__(**kwargs)
         self._n_classes = num_classes
         self._s = s
@@ -100,11 +108,9 @@ class ArcFace(Layer):
     def call(self, inputs, training=None):
         embedding, label = inputs
 
-        # Normalize features and weights
-        x = tf.nn.l2_normalize(embedding)
-        w = tf.nn.l2_normalize(self._w)
-
-        # Dot product
+        # Normalize features and weights and compute dot product
+        x = tf.nn.l2_normalize(embedding, axis=1)
+        w = tf.nn.l2_normalize(self._w, axis=0)
         logits = tf.matmul(x, w)
 
         training = _resolve_training(self, training)
@@ -125,18 +131,139 @@ class ArcFace(Layer):
 
 
 class AdaCos(Layer):
-    def __init__(self, num_classes, m=0.5, regularizer=None, **kwargs):
+    """
+    Implementation of AdaCos layer.
+
+    References:
+      Original Paper: https://arxiv.org/abs/1905.00292
+      PyTorch Implementation:
+        https://github.com/4uiiurz1/pytorch-adacos/blob/master/metrics.py
+        Note that this implementation has flaws (at least I think so)
+      TensorFlow 1.x Implementation:
+        https://github.com/taekwan-lee/adacos-tensorflow/blob/master/adacos.py
+      Keras Implementation with TensorFlow 1.x backend:
+        https://github.com/DaisukeIshibe/Keras-Adacos/
+    """
+    def __init__(self,
+                 num_classes,
+                 m=0.5,
+                 is_dynamic=False,
+                 regularizer=None,
+                 **kwargs):
+
         super().__init__(**kwargs)
         self._n_classes = num_classes
-        self._s = math.sqrt(2) * math.log(num_classes - 1)
+        self._init_s = math.sqrt(2) * math.log(num_classes - 1)
+        self._m = m
+        self._is_dynamic = is_dynamic
+        self._regularizer = regularizer
+
+    def build(self, input_shape):
+        embedding_shape, label_shape = input_shape
+        self._w = self.add_weight(shape=(embedding_shape[-1], self._n_classes),
+                                  initializer='glorot_uniform',
+                                  trainable=True,
+                                  regularizer=self._regularizer)
+        if self._is_dynamic:
+            self._s = self.add_weight(shape=(),
+                                      initializer=Constant(self._init_s),
+                                      trainable=False,
+                                      aggregation=tf.VariableAggregation.MEAN)
+
+    def call(self, inputs, training=None):
+        embedding, label = inputs
+
+        # Normalize features and weights and compute dot product
+        x = tf.nn.l2_normalize(embedding, axis=1)
+        w = tf.nn.l2_normalize(self._w, axis=0)
+        logits = tf.matmul(x, w)
+
+        # Fixed AdaCos
+        is_dynamic = tf_utils.constant_value(self._is_dynamic)
+        if not is_dynamic:
+            # _s is not created since we are not in dynamic mode
+            output = tf.multiply(self._init_s, logits)
+            return output
+
+        training = _resolve_training(self, training)
+        if not training:
+            # We don't have labels to update _s if we're not in training mode
+            return self._s * logits
+        else:
+            theta = tf.math.acos(
+                    K.clip(logits, -1.0 + K.epsilon(), 1.0 - K.epsilon()))
+            one_hot = tf.one_hot(label, depth=self._n_classes)
+            b_avg = tf.where(one_hot < 1.0,
+                             tf.exp(self._s * logits),
+                             tf.zeros_like(logits))
+            b_avg = tf.reduce_mean(tf.reduce_sum(b_avg, axis=1))
+            theta_class = tf.gather_nd(
+                    theta,
+                    tf.stack([
+                        tf.range(tf.shape(label)[0]),
+                        tf.cast(label, tf.int32)
+                    ], axis=1))
+            mid_index = tf.shape(theta_class)[0] // 2 + 1
+            theta_med = tf.nn.top_k(theta_class, mid_index).values[-1]
+
+            # Since _s is not trainable, this assignment is safe. Also,
+            # tf.function ensures that this will run in the right order.
+            self._s.assign(
+                    tf.math.log(b_avg) /
+                    tf.math.cos(tf.minimum(math.pi/4, theta_med)))
+
+            # Return scaled logits
+            return self._s * logits
+
+
+class CosFace(Layer):
+    """
+    Implementation of CosFace layer.
+
+    References:
+      Original paper: https://arxiv.org/abs/1801.09414
+      PyTorch implementation:
+        https://github.com/4uiiurz1/pytorch-adacos/blob/master/metrics.py
+      Keras for Tensorflow 1.x implementation:
+        https://github.com/4uiiurz1/keras-arcface/blob/master/metrics.py
+    """
+    def __init__(self,
+                 num_classes=10,
+                 s=30.0,
+                 m=0.35,
+                 regularizer=None,
+                 **kwargs):
+
+        super().__init__(**kwargs)
+        self._n_classes = num_classes
+        self._s = s
         self._m = m
         self._regularizer = regularizer
 
     def build(self, input_shape):
-        embedding_shape, label = input_shape
+        embedding_shape, label_shape = input_shape
+        self._w = self.add_weight(shape=(embedding_shape[-1], self._n_classes),
+                                  initializer='glorot_uniform',
+                                  trainable=True,
+                                  regularizer=self._regularizer)
 
-    def call(self, inputs):
-        pass
+    def call(self, inputs, training=None):
+        embedding, label = inputs
+
+        # Normalize features and weights and compute dot product
+        x = tf.nn.l2_normalize(embedding, axis=1)
+        w = tf.nn.l2_normalize(self._w, axis=0)
+        logits = tf.matmul(x, w)
+
+        training = _resolve_training(self, training)
+        if not training:
+            # We don't have labels to update _s if we're not in training mode
+            return self._s * logits
+        else:
+            target_logits = logits - self._m
+            one_hot = tf.one_hot(label, depth=self._n_classes)
+            output = logits * (1. - one_hot) + target_logits * one_hot
+            return self._s * output
 
 
 def _resolve_training(layer, training):
@@ -145,7 +272,6 @@ def _resolve_training(layer, training):
     if isinstance(training, int):
         training = bool(training)
     if not layer.trainable:
-        # When the layer is not trainable, it overrides the value passed from
-        # model.
+        # When the layer is not trainable, override the value
         training = False
     return tf_utils.constant_value(training)
